3493c9a614c727eef14d7f9bdd7b8cf8cd4fefaa
mleger45 <mleger45@gmail.com>
2021-04-24 02:42:58 -0300

    removing the Secret key from repo

--- lvdmbackend/lvdmbackend/settings.py
+++ lvdmbackend/lvdmbackend/settings.py
@@ -15,13 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 print(BASE_DIR)
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96q372ltwmp7y9$d5e@hkmpbqia!s=v(^lg08l7$qm0)=x5uq0'
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
